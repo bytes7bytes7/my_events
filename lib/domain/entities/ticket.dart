49565af3fa5a3073b90a069b8a61db259bd3207a
bytes7bytes7 <bytes7bytes7@gmail.com>
2023-08-13 20:41:02 +0300

    create Ticket

---
+++ lib/domain/entities/ticket.dart
@@ -0,0 +1,11 @@
+class Ticket {
+  const Ticket({
+    required this.id,
+    required this.role,
+    required this.eventID,
+  });
+
+  final String id;
+  final String role;
+  final String eventID;
+}
